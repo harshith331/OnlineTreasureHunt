22c66a338794ea7c9cf0d9fbb951576b96861a9f
Nirvik <nirvik.agarwal@gmail.com>
2020-08-10 17:18:06 +0530

    changed settings

--- oth/settings.py
+++ oth/settings.py
@@ -24,7 +24,7 @@ template_dir=os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
