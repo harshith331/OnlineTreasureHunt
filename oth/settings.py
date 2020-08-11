9425b6c972204f0b91eb68441e6349e1f30adc10
Nirvik <nirvik.agarwal@gmail.com>
2020-08-11 15:06:39 +0530

    whitenoise

--- oth/settings.py
+++ oth/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
 
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_root")
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL ='/media/'
 
